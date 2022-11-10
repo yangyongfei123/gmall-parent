c6980aea4bd04a4d747099c3de079c5a5edd1fe4
yyf <1217473440@qq.com>
2022-11-10 22:38:19 +0800

    2022.11.10更新

--- web/web-all/src/main/java/com/atguigu/gmall/web/controller/PassportController.java
+++ web/web-all/src/main/java/com/atguigu/gmall/web/controller/PassportController.java
@@ -1,9 +1,15 @@
 package com.atguigu.gmall.web.controller;
 
+import com.atguigu.gmall.common.result.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+
 // * 用户认证接口
 @Controller
 public class PassportController {
@@ -12,5 +18,10 @@ public class PassportController {
         String originUrl = request.getParameter("originUrl");
         request.setAttribute("originUrl", originUrl);
         return "login";
+    }
+
+    @PostMapping("123456")
+    public Result get() {
+        return Result.ok();
     }
 }

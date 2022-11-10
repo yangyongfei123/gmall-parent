977ddf602a202a37d942b3f5bb3fa09df80e8c0a
yyf <1217473440@qq.com>
2022-11-10 22:30:14 +0800

    2022.11.10更新

--- ware-manage/src/main/java/com/atguigu/gmall/ware/ServiceWareApplication.java
+++ ware-manage/src/main/java/com/atguigu/gmall/ware/ServiceWareApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan({"com.atguigu.gmall"})
 @EnableDiscoveryClient
+@RestController
 public class ServiceWareApplication {
 
 	public static void main(String[] args) {
